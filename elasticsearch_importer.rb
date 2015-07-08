1eb92abddd9527e497f62aa38efdba9d8aeeedc8
Tom Stuart <tom@codon.com>
2015-07-08 12:15:09 +0100

    Add rake task to import records into Elasticsearch

---
+++ elasticsearch_importer.rb
@@ -0,0 +1,39 @@
+require 'elasticsearch'
+require_relative 'models/record'
+
+class ElasticsearchImporter
+  def import!
+    establish_connection
+    client = Elasticsearch::Client.new url: ENV['SEARCHBOX_URL']
+
+    total = 0
+    puts "Importing records into Elasticsearch…"
+
+    Record.find_in_batches do |records|
+      puts "Importing records #{total + 1}–#{total + records.length}…"
+      total = total + records.length
+
+      body =
+        records.map { |record|
+          {
+            index: {
+              _index: 'library',
+              _type:  'record',
+              _id:    record.identifier,
+              data:   record.metadata.merge(record.attributes.slice(:title, :leader))
+            }
+          }
+        }
+
+      client.bulk body: body
+    end
+
+    puts "Finished importing #{total} records"
+  end
+
+  private
+
+  def establish_connection
+    ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+  end
+end
--- rakefile.rb
+++ rakefile.rb
@@ -3,12 +3,17 @@ require 'active_support/core_ext'
 require 'pg'
 
 require_relative 'marc_ingester'
+require_relative 'elasticsearch_importer'
 
 desc "Ingest MARC XML files"
 task :ingest do
   MarcIngester.new.ingest!
 end
 
+desc "Import records into Elasticsearch"
+task :import do
+  ElasticsearchImporter.new.import!
+end
 
 namespace :db do
 
