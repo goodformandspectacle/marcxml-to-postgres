b7e0ffb81e2667961dafbea1d6b14a46c0042d98
Tom Stuart <tom@codon.com>
2015-07-09 12:55:12 +0100

    Remove rake db:migrate task

--- rakefile.rb
+++ rakefile.rb
@@ -15,13 +15,3 @@ task :import do
   $stdout.sync = true
   ElasticsearchImporter.new.import!
 end
-
-namespace :db do
-
-  desc "Run migrations"
-  task :migrate do
-    ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
-    ActiveRecord::Migrator.migrate('db/migrate', ENV["VERSION"] ? ENV["VERSION"].to_i : nil)
-  end
-
-end
